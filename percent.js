59b54d64f6ae36c8cdb90bf802773eab0a23af88
mubeen070 <71404765+mubeen070@users.noreply.github.com>
2020-10-25 23:29:49 +0500

    Update percent.js

--- percent.js
+++ percent.js
@@ -9,6 +9,6 @@ function percentage()
 			var a = Number(document.getElementById("t1").value);
 			var b = Number(document.getElementById("t2").value) / 100;
 			$("#Result").text(a*b);
-			$("#Result").addClass('animated bounceOutLeft')
+			
 		}
 	
